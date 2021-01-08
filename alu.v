f219e83c4ddb5b5a2d6f24dd7fcfcfb05ddc05ad
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-08 22:31:43 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -17,11 +17,11 @@ always @(*)
           begin
           case(alusignal)
 
-5'b00000 : aluresult = a + b ;// add
-5'b00001 : aluresult = a - b ; // sub
-5'b00010 : aluresult = a * b ; //mul
-5'b00011 : aluresult = a / b ; //div 
-5'b00100 : aluresult = a % b ; //mod
+5'b00000 : aluresult = a + b ;					// add
+5'b00001 : aluresult = a - b ; 					// sub
+5'b00010 : aluresult = a * b ; 					// mul
+5'b00011 : aluresult = a / b ; 					// div 
+5'b00100 : aluresult = a % b ; 					// mod
 5'b00101 : begin
                 if (a == b)
                 begin 
@@ -38,20 +38,20 @@ always @(*)
                 flags[0] = 1'b0; 
                 flags[1] = 1'b0;
                 end
-           end                        //cmp
-5'b00110 : aluresult = a & b ; //and
-5'b00111 : aluresult = a | b ; //or
-5'b01000 : aluresult = ~a ;    //not
-5'b01001 : aluresult = b ;    // mov
-5'b01010 : aluresult = a << b ;   //lsl
-5'b01011 : aluresult = a >> b ; // a unsigned   //lsr
+           end                        				          // cmp
+5'b00110 : aluresult = a & b ; 					// and
+5'b00111 : aluresult = a | b ; 					// or
+5'b01000 : aluresult = ~a ;    					// not
+5'b01001 : aluresult = b ;    					// mov
+5'b01010 : aluresult = a << b ;   				          // lsl
+5'b01011 : aluresult = a >> b ; 				          // a unsigned   //lsr
 5'b01100 : 
           begin
-          aluresult= a_signed >>> b;                     // a signed     //asr
+          aluresult= a_signed >>> b;                     	                    // a signed     //asr
           end
 5'b01101:
           begin
-                             // nop
+                             					          // nop
           
           end
                    
